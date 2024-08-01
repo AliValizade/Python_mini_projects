ba9e6705f6a41617b95b55cac56f757d95bd2b19
AliValizade <alivalizade@gmail.com>
2024-08-01 10:18:47 +0330

    ObjectOriented add, eq, iadd magic method

--- ObjectOriented/BancAccount.py
+++ ObjectOriented/BancAccount.py
@@ -12,6 +12,19 @@ class BankAccount:
     def __repr__(self):
         return f'{self.__class__.__name__}(name={self.name}, balance={self.balance})'
     
+    def __add__(self, other):
+        balances = self.balance + other.balance
+        names = self.name + '/' + other.name
+        return BankAccount(names, balances)
+    
+    def __eq__(self, other):
+        return self.balance == other.balance
+    
+    def __iadd__(self, other):
+        self.balance += other.balance
+        other.balance = 0
+        print(f'All balance of {other.name} transferd to {self.name}.✅')
+        return self
     
     
 
@@ -24,3 +37,11 @@ c = BankAccount('kia')
 print(a)
 print(b)
 print(repr(c))
+print('='*10)
+print(repr(a + b))
+print(a == b)
+print('='*10)
+a += b
+print(a)
+print(b)
+print('='*10)
